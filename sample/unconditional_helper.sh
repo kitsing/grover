de6ed4483a8234600b18d4dff388372ac8e39fd4
Chu-Cheng Lin <kitsing@fb.com>
2019-10-29 15:18:15 -0700

    seeding

--- sample/unconditional_helper.sh
+++ sample/unconditional_helper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-UNCOND_OUTPUT_DIR="/checkpoint/kitsing/grover/unconditional_samples_dev/${SLURM_NODEID}"
+UNCOND_OUTPUT_DIR="/checkpoint/kitsing/grover/unconditional_samples_dev/${SLURM_PROCID}"
 mkdir -p ${UNCOND_OUTPUT_DIR}
-PYTHONPATH=$(pwd) python sample/unconditional_generate.py -model_config_fn lm/configs/base.json -model_ckpt /checkpoint/kitsing/grover-models/base/model.ckpt -batch_size 1000000 -dir ${UNCOND_OUTPUT_DIR} -max_batch_size 64
+PYTHONPATH=$(pwd) python sample/unconditional_generate.py -model_config_fn lm/configs/base.json -model_ckpt /checkpoint/kitsing/grover-models/base/model.ckpt -batch_size 1000000 -dir ${UNCOND_OUTPUT_DIR} -max_batch_size 64 --seed 42
