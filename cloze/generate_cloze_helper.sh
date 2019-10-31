555be2cbc34b956366b112b1eb1c309a2bea5fc5
Chu-Cheng Lin <kitsing@fb.com>
2019-10-30 23:34:55 -0700

    seeding

--- cloze/generate_cloze_helper.sh
+++ cloze/generate_cloze_helper.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-FNAME=ls /checkpoint/kitsing/grover/tfrecords/preprocessed_val$(printf "%04d" ${SLURM_PROCID}).tfrecord
+FORMAT_NUM=$(printf "%04d" ${SLURM_PROCID})
+FNAME="/checkpoint/kitsing/grover/tfrecords/preprocessed_val${FORMAT_NUM}.tfrecord"
 OUTPUT_PATH=${1}
 CHOICE=${2}
 PYTHONPATH=$(pwd) python cloze/generate_cloze_dataset.py --file ${FNAME} --choice ${CHOICE} --output-path ${OUTPUT_PATH}
