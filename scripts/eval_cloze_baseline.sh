0e3a8a616248535c20a1693322bb20ec1a3332d9
Chu-Cheng Lin <chu.cheng.lin@gmail.com>
2019-11-06 11:22:33 -0800

    sync

---
+++ scripts/eval_cloze_baseline.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+### Section1: SBATCH directives to specify job configuration
+## job name
+#SBATCH --job-name=eval-cloze-baseline
+## filename for job standard output (stdout)
+## %j is the job id, %u is the user id
+#SBATCH --output=/checkpoint/%u/jobs/grover/eval-cloze-baseline-%j.out
+## filename for job standard error output (stderr)
+#SBATCH --error=/checkpoint/%u/jobs/grover/eval-cloze-baseline-%j.err
+#SBATCH --time=2880
+
+## partition name
+#SBATCH --partition=learnfair
+## number of nodes
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=80   # 80/8 cpus per task
+## number of tasks per node
+#SBATCH --gres=gpu:8
+#SBATCH --constraint=volta32gb
+export PYTHONPATH=$(pwd)
+
+model_type="base"
+OUTPUT_DIR=${1} # put your output directory here
+input_dev_file='/checkpoint/kitsing/grover/cloze/preprocessed_val0[0-5]*.tfrecord.npz'
+
+# Make sure batch size scales.
+let batch_size=160
+
+# NODE_LIST=$( scontrol show hostname ${SLURM_JOB_NODELIST} | sed -z 's/\n/\:8,/g' )
+# NODE_LIST=${NODE_LIST%?}
+
+# export TF_XLA_FLAGS="--tf_xla_auto_jit=2 --tf_xla_cpu_global_jit"
+export TF_XLA_FLAGS="--tf_xla_auto_jit=2"
+export XLA_FLAGS="--xla_gpu_cuda_data_dir=${CUDA_HOME}"
+
+set -o noglob
+RUN_STRING="python lm/eval_nce.py \
+--model-config-fn lm/configs/${model_type}.json \
+--gen-model-ckpt /checkpoint/kitsing/grover-models/${model_type}/model.ckpt \
+--dis-model-ckpt /checkpoint/kitsing/grover-models/discriminator_nov_1/model.ckpt-133000 \
+--batch-size ${batch_size} \
+--files ${input_dev_file} \
+--output-path ${OUTPUT_DIR} \
+--num-gpus 8 --baseline"
+srun --label ${RUN_STRING}
+set +o noglob
---
+++ scripts/eval_cloze_priority.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+### Section1: SBATCH directives to specify job configuration
+## job name
+#SBATCH --job-name=eval-cloze
+## filename for job standard output (stdout)
+## %j is the job id, %u is the user id
+#SBATCH --output=/checkpoint/%u/jobs/grover/eval-cloze-%j.out
+## filename for job standard error output (stderr)
+#SBATCH --error=/checkpoint/%u/jobs/grover/eval-cloze-%j.err
+#SBATCH --time=2880
+
+## partition name
+#SBATCH --partition=priority
+## number of nodes
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=80   # 80/8 cpus per task
+## number of tasks per node
+#SBATCH --gres=gpu:8
+#SBATCH --constraint=volta32gb
+#SBATCH --comment="acl 2020 submission: need this quick evaluation to decide whether we stop training one particular variant"
+export PYTHONPATH=$(pwd)
+
+OUTPUT_DIR=${1} # put your output directory here
+mkdir -p ${OUTPUT_DIR}
+DIS_MODEL_CKPT=${2}
+model_type=${3}
+gen_model_type="base"
+#input_dev_file='/checkpoint/kitsing/grover/cloze/preprocessed_val0[0-5]*.tfrecord.npz'
+input_dev_file='/checkpoint/kitsing/grover/cloze/preprocessed_val00[0-9]*.tfrecord.npz'
+
+# Make sure batch size scales.
+let batch_size=160
+
+# NODE_LIST=$( scontrol show hostname ${SLURM_JOB_NODELIST} | sed -z 's/\n/\:8,/g' )
+# NODE_LIST=${NODE_LIST%?}
+
+# export TF_XLA_FLAGS="--tf_xla_auto_jit=2 --tf_xla_cpu_global_jit"
+export TF_XLA_FLAGS="--tf_xla_auto_jit=2"
+export XLA_FLAGS="--xla_gpu_cuda_data_dir=${CUDA_HOME}"
+
+set -o noglob
+RUN_STRING="python lm/eval_nce.py \
+--model-config-fn lm/configs/${model_type}.json \
+--gen-model-ckpt /checkpoint/kitsing/grover-models/${gen_model_type}/model.ckpt \
+--dis-model-ckpt ${DIS_MODEL_CKPT} \
+--batch-size ${batch_size} \
+--files ${input_dev_file} \
+--output-path ${OUTPUT_DIR} \
+--num-gpus 8"
+srun --label ${RUN_STRING}
+set +o noglob
