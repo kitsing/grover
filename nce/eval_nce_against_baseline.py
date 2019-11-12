3740929273c81922125f1d5b1c506e481de2a645
Chu-Cheng Lin <kitsing@fb.com>
2019-11-11 22:55:16 -0800

    noise

--- nce/eval_nce_against_baseline.py
+++ nce/eval_nce_against_baseline.py
@@ -1,26 +1,92 @@
 #!/usr/bin/env python3
 import numpy as np
 from scipy.special import logsumexp
+import tensorflow as tf
 
-def compute_nce_probs(inp, noises):
+
+def compute_nce_probs(inp, noise_probs, noise_probs_under_model):
     loaded = np.load(inp)
-    loaded_noise = np.load(noises)
-    noise_weighted = loaded_noise['noise_probs_under_model'] - loaded_noise['noise_probs_under_noise']
+    noise_weighted = noise_probs_under_model - noise_probs
     true_weighted = loaded['input_probs_under_model'] - loaded['input_probs_under_noise']
     z = np.concatenate(
         (true_weighted.reshape((-1, 1)), np.tile(noise_weighted.reshape((1, -1)), (true_weighted.shape[0], 1))), axis=1)
     probs = true_weighted - logsumexp(z, axis=1)
     return probs
 
+
+def get_all_noises(noise_files):
+    all_noises = []
+    all_noise_probs = []
+    for f in noise_files:
+        loaded_noise = np.load(f)
+        all_noises.append(loaded_noise['noise_tokens'])
+        all_noise_probs.append(loaded_noise['noise_probs_under_noise'])
+    return np.concatenate(all_noises, axis=0), np.concatenate(all_noise_probs, axis=0)
+
+
+def compute_prob_under_model(inp, noise_model_config, model_config, batch_size_per_chunk, num_gpus, seq_length,
+                             gen_ckpt, dis_ckpt):
+    from sample.encoder import get_encoder
+    from lm.modeling import GroverConfig, sample
+    from nce.calculate_nce_loss import eval_seq, restore, get_seq_probs
+    encoder = get_encoder()
+    news_config = GroverConfig.from_json_file(model_config)
+
+    tf_config = tf.ConfigProto(allow_soft_placement=True)
+
+    with tf.Session(config=tf_config, graph=tf.Graph()) as sess:
+        all_tokens = []
+        all_probs = []
+
+        ignore_ids = tf.placeholder(tf.bool, [news_config.vocab_size])
+        ignore_ids_np = np.array(encoder.special_tokens_onehot)
+        ignore_ids_np[encoder.__dict__['end_article']] = 0
+        for i in range(num_gpus):
+            with tf.device('/gpu:' + str(i)):
+                # actual examples
+                tokens = tf.placeholder(tf.int32, [batch_size_per_chunk, seq_length])
+                all_tokens.append(tokens)
+                probs = tf.stop_gradient(eval_seq(news_config, tokens, 1., baseline=False,
+                                                  ignore_ids=ignore_ids, gen_config=noise_model_config))
+                all_probs.append(probs)
+
+
+        with tf.device('/cpu:0'):
+            merged_probs = tf.concat(all_probs, axis=0)
+
+        restore('gen', gen_ckpt)
+        restore('dis', dis_ckpt)
+
+        probs_under_model = get_seq_probs(seqs=inp,
+                                          batch_size=batch_size_per_chunk * num_gpus,
+                                          token_place_holders=all_tokens,
+                                          num_gpus=num_gpus,
+                                          tf_outputs=merged_probs,
+                                          ignore_ids_np=ignore_ids_np,
+                                          ignore_ids=ignore_ids)
+        return probs_under_model
+
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--inp')
     parser.add_argument('--noises')
-
+    parser.add_argument('--noise-model-config', default='/private/home/kitsing/git/grover/lm/configs/base.json')
+    parser.add_argument('--model-config', default='/private/home/kitsing/git/grover/lm/configs/base.json')
+    parser.add_argument('--batch-size', default=8)
+    parser.add_argument('--seq-length', default=1025)
+    parser.add_argument('--num-gpus', default=8)
+    parser.add_argument('--gen-ckpt', default='/checkpoint/kitsing/grover-models/base/model.ckpt')
+    parser.add_argument('--dis-ckpt', default='/checkpoint/kitsing/grover-models/base/model.ckpt')
     args = parser.parse_args()
-
-    probs = compute_nce_probs(args.inp, args.noises)
+    from glob import glob
+    noise_files = glob(args.noises)
+    noise_tokens, noise_probs = get_all_noises(noise_files)
+    noise_probs_under_model = compute_prob_under_model(noise_tokens, args.noise_model_config, args.model_config,
+                                                       args.batch_size, args.num_gpus, args.seq_length, args.gen_ckpt,
+                                                       args.dis_ckpt)
+    probs = compute_nce_probs(args.inp, noise_probs, noise_probs_under_model)
     print(probs)
     greater_than_chance = probs > - np.log(probs.shape[1])
     print(greater_than_chance)
---
+++ nce/gen_noise_samples.py
@@ -0,0 +1,131 @@
+import tensorflow as tf
+import numpy as np
+import sys
+import argparse
+from glob import glob
+
+sys.path.append('../')
+from lm.modeling import GroverConfig, eval_seq, sample
+from sample.encoder import get_encoder
+from tqdm import tqdm
+from os import environ
+from os.path import basename
+from random import seed as rnd_seed
+from scipy.special import logsumexp
+from math import ceil
+from nce.calculate_nce_loss import eval_seq, get_seq_probs, restore
+
+parser = argparse.ArgumentParser(description='Evaluation')
+
+parser.add_argument(
+    '--noise-model-config-fn',
+    default='../lm/configs/base.json',
+    type=str,
+    help='Configuration JSON for the noise model',
+)
+parser.add_argument(
+    '--noise-model-ckpt',
+    default='',
+    type=str,
+    help='discriminator checkpoint file for the model',
+)
+parser.add_argument(
+    '--batch-size',
+    default=50,
+    type=int,
+    help='How many things to generate per context. will split into chunks if need be',
+)
+parser.add_argument('--num-noise-chunks', default=1, type=int)
+parser.add_argument('--files', default='input.npz', type=str)
+parser.add_argument('--seed', default=42, type=int)
+parser.add_argument('--correction-factor', default=1., type=float) # correction factor: 14136832/13628509
+parser.add_argument('--seq-length', default=1025, type=int)
+parser.add_argument('--output-path', default='./', type=str)
+parser.add_argument('--noise-output-path', default='./', type=str)
+parser.add_argument('--num-gpus', default=8, type=int)
+
+args = parser.parse_args()
+args.fold = int(environ['SLURM_PROCID'])
+args.num_folds = int(environ['SLURM_NTASKS'])
+from glob import glob
+
+all_files = list(glob(args.files))[:]
+
+args.num_files_per_node = int(ceil(len(all_files) / args.num_folds))
+args.our_files = all_files[args.fold * args.num_files_per_node: (args.fold+1) * args.num_files_per_node]
+
+# for training we are keeping the seed the same across all runs
+seed = args.seed
+
+rnd_seed(seed)
+tf.set_random_seed(seed)
+
+encoder = get_encoder()
+news_config = GroverConfig.from_json_file(args.model_config_fn)
+noise_news_config = GroverConfig.from_json_file(args.noise_model_config_fn)
+
+print('start: {}'.format(encoder.__dict__['begin_article']))
+print('end: {}'.format(encoder.__dict__['end_article']))
+
+tf_config = tf.ConfigProto(allow_soft_placement=True)
+
+context_formatted = [encoder.__dict__['begin_article'], ]
+
+with tf.Session(config=tf_config, graph=tf.Graph()) as sess:
+    batch_size_per_chunk = args.batch_size
+    all_noise_probs = []
+
+    all_sampled_tokens = []
+    all_sampled_probs = []
+
+    initial_context = tf.placeholder(tf.int32, [batch_size_per_chunk, None])
+    p_for_topp = tf.placeholder(tf.float32, [batch_size_per_chunk])
+    eos_token = tf.placeholder(tf.int32, [])
+    ignore_ids = tf.placeholder(tf.bool, [news_config.vocab_size])
+    ignore_ids_np = np.array(encoder.special_tokens_onehot)
+    ignore_ids_np[encoder.__dict__['end_article']] = 0
+    for i in range(args.num_gpus):
+        with tf.device('/gpu:' + str(i)):
+            # sampled noises
+            sampled_tokens, sampled_probs = sample(news_config=noise_news_config, initial_context=initial_context,
+                                                   eos_token=eos_token, ignore_ids=ignore_ids, p_for_topp=p_for_topp,
+                                                   do_topk=False, seed=i, max_out_tensor=True, vanilla=True)
+            all_sampled_tokens.append(sampled_tokens)
+            all_sampled_probs.append(sampled_probs)
+
+    with tf.device('/cpu:0'):
+        merged_sampled_probs = tf.concat(all_sampled_probs, axis=0)
+        merged_sampled_tokens = tf.concat(all_sampled_tokens, axis=0)
+
+    restore('newslm', args.noise_model_ckpt)
+
+    # get noise samples first
+    noise_token_chunks = []
+    noise_prob_chunks = []
+    for chunk in tqdm(range(args.num_noise_chunks), disable=None):
+        noise_token_chunk, noise_prob_chunk = sess.run([merged_sampled_tokens, merged_sampled_probs],
+                                                       feed_dict={
+                                                           initial_context: [context_formatted] * batch_size_per_chunk,
+                                                           eos_token: encoder.__dict__['end_article'],
+                                                           ignore_ids: ignore_ids_np,
+                                                           p_for_topp: np.ones((batch_size_per_chunk,),
+                                                                               dtype=np.float32)}
+                                                       )
+        eos_positions = np.argmax(noise_token_chunk == encoder.__dict__['end_article'], axis=1)
+        valid_seqs = (eos_positions != 0)
+        mask = np.tile(np.arange(1025, dtype=np.int32)[None, :], (eos_positions.shape[0], 1))
+        masked = mask <= eos_positions[:, None]
+        noise_token_chunk = np.where(masked, noise_token_chunk, encoder.padding)[valid_seqs]
+        noise_token_chunks.append(noise_token_chunk)
+
+        prob_mask = masked[:, 1:]
+        prob_masked = (prob_mask * noise_prob_chunk)[valid_seqs]
+        noise_prob_masked = np.sum(prob_masked, axis=1)
+        assert prob_masked.shape[0] == noise_token_chunk.shape[0]
+        noise_prob_chunks.append(noise_prob_masked)
+    noise_tokens = np.concatenate(noise_token_chunks, axis=0)
+    n_probs = np.concatenate(noise_prob_chunks, axis=0)
+
+    noise_output_fname = f'{args.noise_output_path}/{args.fold}.noise.npz'
+    np.savez(noise_output_fname, noise_tokens=noise_tokens,
+             noise_probs_under_noise=np.reshape(n_probs, (-1,)))
