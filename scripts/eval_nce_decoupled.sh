f5e85f587794062161993563e9359852d191f303
Chu-Cheng Lin <kitsing@fb.com>
2019-11-12 21:25:37 -0800

    noise

--- scripts/eval_nce_decoupled.sh
+++ scripts/eval_nce_decoupled.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 NITER=${1}
-mkdir -p /checkpoint/kitsing/grover/nce-${NITER}
-rm -rf /checkpoint/kitsing/grover/nce-${NITER}/*
+model=${2}
+ID=${3}
+OUTPUT_DIR=/checkpoint/kitsing/grover/${ID}/nce-${NITER}
+mkdir -p ${OUTPUT_DIR}
 
-PYTHONPATH="." python nce/calculate_nce_loss.py --model-config-fn lm/configs/base_mask_padding_sum.json --noise-model-config-fn lm/configs/base.json --gen-model-ckpt /checkpoint/kitsing/grover-models/base/model.ckpt --dis-model-ckpt /checkpoint/kitsing/grover-models/discriminator-debugging/model.ckpt-${NITER} --noise-model-ckpt /checkpoint/kitsing/grover-models/base/model.ckpt --batch-size 8 --num-noise-chunks 1 --file /checkpoint/kitsing/grover/nce/0.npz --output-path /checkpoint/kitsing/grover/nce-${NITER} --num-gpus 8  --noise-output-path /checkpoint/kitsing/grover/nce/noises/${NITER}
+PYTHONPATH="." python nce/calculate_nce_loss.py --model-config-fn lm/configs/${model}.json --noise-model-config-fn lm/configs/base.json --gen-model-ckpt /checkpoint/kitsing/grover-models/base/model.ckpt --dis-model-ckpt /checkpoint/kitsing/grover-models/discriminator-${ID}/model.ckpt-${NITER} --noise-model-ckpt /checkpoint/kitsing/grover-models/base/model.ckpt --batch-size 8 --num-noise-chunks 1 --file /checkpoint/kitsing/grover/nce/0.npz --output-path ${OUTPUT_DIR} --num-gpus 8  --noise-output-path /checkpoint/kitsing/grover/nce/noises/${NITER}
