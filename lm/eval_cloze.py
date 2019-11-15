72b18c04986cf3c7364703da86b3a83ae5446dde
Chu-Cheng Lin <kitsing@fb.com>
2019-11-14 19:52:09 -0800

    restore

--- lm/eval_cloze.py
+++ lm/eval_cloze.py
@@ -12,6 +12,7 @@ from os import environ
 from os.path import basename
 from random import seed as rnd_seed
 from math import ceil
+from nce.utils import restore
 
 parser = argparse.ArgumentParser(description='Evaluation')
 parser.add_argument(
@@ -82,43 +83,6 @@ print('end: {}'.format(encoder.__dict__['end_article']))
 tf_config = tf.ConfigProto(allow_soft_placement=True)
 
 
-def restore(scope, checkpoint):
-    vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=scope)
-    checkpoint_vars = tf.train.list_variables(checkpoint)
-    checkpoint_names = [_[0] for _ in checkpoint_vars]
-    assignment_map = dict()
-    unused_vars_in_checkpoint = set(checkpoint_names)
-    for var in vars:
-        name = var.name
-        assert name.endswith(':0')
-        name = name[:-2]
-
-        # hack
-        if name.startswith('discriminator_final_layer'):
-            if name in unused_vars_in_checkpoint:
-                assignment_map[name] = var
-            else:
-                tf.logging.warn(f'key not found: {name}')
-            continue
-
-        splitted_name = name.split(scope)
-        if len(splitted_name) > 1:
-            new_name = ''.join(['newslm'] + splitted_name[1:])
-            if new_name in unused_vars_in_checkpoint:
-                assignment_map[new_name] = var
-                tf.logging.info(f'key found: {new_name} -> {name}')
-                unused_vars_in_checkpoint.remove(new_name)
-            else:
-                tf.logging.warn(f'key not found: {new_name}')
-        else:
-            tf.logging.warn(f'key {name} does not start with {scope}')
-
-    tf.logging.warn(f'unused variables in checkpoint: {unused_vars_in_checkpoint}')
-    # print(gen_assignment_map)
-    saver = tf.train.Saver(var_list=assignment_map)
-    saver.restore(sess, checkpoint)
-
-
 with tf.Session(config=tf_config, graph=tf.Graph()) as sess:
     all_tokens = []
 
@@ -137,9 +101,9 @@ with tf.Session(config=tf_config, graph=tf.Graph()) as sess:
     with tf.device('/cpu:0'):
         merged_probs = tf.concat(all_probs, axis=0)
 
-    restore('gen', args.gen_model_ckpt)
+    restore('gen', args.gen_model_ckpt, sess)
     if not args.baseline:
-        restore('dis', args.dis_model_ckpt)
+        restore('dis', args.dis_model_ckpt, sess)
 
     # Let's go!
     for f in tqdm(our_files, disable=None):
